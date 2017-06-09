e24e615d6a13a66d872a951112b37d2df3a17ed8
BHess2653 <GM_Dahaka@BHMac.local>
2017-06-09 17:40:13 -0400

    added base reducers and actions

--- actions/index.js
+++ actions/index.js
@@ -1,1 +1,30 @@
-// comment
+export const getClubs = () => {
+  return {
+    type: "GET_CLUBS"
+  }
+}
+
+export const selectClub = (club) => {
+  return {
+    type: "SELECT_CLUB",
+    club
+  }
+}
+
+export const showClubs = () => {
+  return {
+    type: "SHOW_CLUBS"
+  }
+}
+
+export const orderByName = () => {
+  return {
+    type: "ORDER_BY_NAME"
+  }
+}
+
+export const orderByStanding = (clubs) => {
+  return {
+    type: "ORDER_BY_STANDING"
+  }
+}
---
+++ reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+import eplclubs from '../data/eplclubs'
+
+const clubdReducer = combineReducers({
+  eplclubs
+})
+
+export default clubdReducer
